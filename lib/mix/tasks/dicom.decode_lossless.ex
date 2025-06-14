044cd84a296a1b57398d475001b5bde566bca15b
Leonardo M. Ramé <martinrame@yahoo.com>
2025-06-14 12:29:50 +0000

    Add output file to JPEG Lossless decoder task

--- lib/mix/tasks/dicom.decode_lossless.ex
+++ lib/mix/tasks/dicom.decode_lossless.ex
@@ -15,10 +15,25 @@ defmodule Mix.Tasks.Dicom.DecodeLossless do
     ds = BinaryFormat.from_file!("test.dcm")
     frames = pixel_frames(ds)
 
-    Enum.each(Enum.with_index(frames, 1), fn {frame, idx} ->
-      decoded = BinaryFormat.decode_jpeg_lossless(frame)
-      IO.puts("Frame #{idx}: #{byte_size(decoded)} bytes decoded")
-    end)
+    decoded_frames =
+      Enum.map(Enum.with_index(frames, 1), fn {frame, idx} ->
+        decoded = BinaryFormat.decode_jpeg_lossless(frame)
+        IO.puts("Frame #{idx}: #{byte_size(decoded)} bytes decoded")
+        decoded
+      end)
+
+    bits_allocated = DataSet.value_for!(ds, :BitsAllocated)
+    vr = if bits_allocated <= 8, do: :OB, else: :OW
+
+    pixel_data = Enum.reduce(decoded_frames, <<>>, fn frame, acc -> acc <> frame end)
+
+    ds =
+      ds
+      |> DataSet.put(:PixelData, vr, [pixel_data])
+      |> DataSet.with_transfer_syntax(:explicit_vr_little_endian)
+
+    data = BinaryFormat.to_file_data(ds)
+    File.write!("test_decoded.dcm", data)
   end
 
   defp pixel_frames(ds) do
